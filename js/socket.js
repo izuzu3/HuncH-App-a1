28686537c9651b9d11a94f6662b1d29395bc1b6a
izuzu3 <godgodgawd@gmail.com>
2017-06-24 11:44:18 +0530

    Update socket.js

--- js/socket.js
+++ js/socket.js
@@ -1,63 +1,106 @@
- var sock = null;
- var ellog = null;
- var i=0;
- window.onload = function() {
-	var wsuri;
-	//wsuri = "ws://192.168.1.10:8500";
-	wsuri = "ws://192.168.1.3:9000";
-	if ("WebSocket" in window) {
-	   sock = new WebSocket(wsuri);
-	} else if ("MozWebSocket" in window) {
-	   sock = new MozWebSocket(wsuri);
-	} else {
-	   alert("Browser does not support WebSocket!");
-	}
-	
-	if (sock) {
-	   sock.onopen = function() {
-		  alert("Connected to " + wsuri);
-	   }
-	   sock.onclose = function(e) {
-		  alert("Connection closed (wasClean = " + e.wasClean + ", code = " + e.code + ", reason = '" + e.reason + "')");
-		  sock = null;
-		  alert("rejected" + i);
-	   }
-	   sock.onmessage = function(e) {
-		  alert("Got echo: " + e.data);
-	   }
-	}
-};
-/*
- function broadcast() {
-	var l1 = document.getElementById("pendt").name;
-	alert(msg + "t0");
-	if (sock) {
-	   sock.send(msg + "t0");
-	   alert("Sent: " + msg);
-	} else {
-	   alert("Not connected.");
-	}
-*/
+var sock=null;
+function findServers(port, ipBase, ipLow, ipHigh, maxInFlight, timeout, cb) {
+    var ipCurrent = +ipLow, numInFlight = 0, servers = [];
+    ipHigh = +ipHigh;
+
+    function tryOne(ip) {
+        ++numInFlight;
+        var address = "ws://" + ipBase + ip + ":" + port;
+        var socket = new WebSocket(address);
+        var timer = setTimeout(function() {
+            console.log(address + " timeout");
+            var s = socket;
+            socket = null;
+            s.close();
+            --numInFlight;
+            next();
+        }, timeout);
+        socket.onopen = function() {
+            if (socket) {
+                alert(address + " success");
+				sock=socket;
+                clearTimeout(timer);
+                servers.push(socket.url);
+				--numInFlight;
+                next();
+            }
+        };
+        socket.onerror = function(err) {
+            if (socket) {
+                console.log(address + " error");
+                clearTimeout(timer);
+                --numInFlight;
+                next();
+            }
+        }
+    }
+
+    function next() {
+        while (ipCurrent <= ipHigh && numInFlight < maxInFlight) {
+            tryOne(ipCurrent++);
+        }
+        // if we get here and there are no requests in flight, then
+        // we must be done
+        if (numInFlight === 0) {
+            console.log(servers);
+            cb(servers);
+        }
+    }
+
+    next();
+}
+
+window.onload = function() {
+	findServers(9000, "192.168.1.", 1, 20, 255, 4000, function(servers) {
+    console.log(servers);
+	});
+}
+
  function l1() {
 	var msg = document.getElementById("pendt").name;
 	//alert(msg);
 	if (sock) {
-	   sock.send(msg);
-	   alert("Sent: " + msg);
+		if(status == 0){				
+			sock.send("L1T4");
+			document.getElementById("pendt").style.backgroundImage = 'url("icons/Button-Pendant-Active.png")';
+			status = 1;
+		}else{
+			sock.send("L1F4");
+			document.getElementById("pendt").style.backgroundImage = 'url("icons/Button-Pendant.png")';			
+			status = 0;
+		}
+	   //alert("Sent: " + msg);
 	} else {
 	   alert("Not connected.");
 	}
+	
+	sock.onmessage = function(e) {
+		//alert("Got echo: " + e.data);
+	}
+
  };
 	
 	
  function l2() {
-	var msg = document.getElementById("vlight").name;
-	alert(msg);
+	var msg = document.getElementById("wlight").name;
 	if (sock) {
-	   sock.send(msg);
-	   alert("Sent: " + msg);
+		if(status == 0){				
+			sock.send("L2T4");
+			document.getElementById("wlight").style.backgroundImage = 'url("icons/Button-Wall-Active.png")';
+			status = 1;
+		}else{
+			sock.send("L2F4");
+			document.getElementById("wlight").style.backgroundImage = 'url("icons/Button-Wall.png")';
+			status = 0;
+		}
+	   //alert("Sent: " + msg);
 	} else {
 	   alert("Not connected.");
 	}
+	
+	sock.onmessage = function(e) {
+		//alert("Got echo: " + e.data);
+	}
+
  };
  
