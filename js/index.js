d64942b7bd408567e3fcc1c12593018c14046541
izuzu3 <godgodgawd@gmail.com>
2017-07-13 11:27:19 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
-$(document).ready(function() {
-
-//MOBILE FOUR
 var menu = "close";
+
+$(document).ready(function() {
 $(".mobile-four .menu-toggle").click(function() {
     
     if (menu === "close") {
       	$(this).parent().next(".mobile-nav").css("transform", "translate(0, 0)");
-     	 menu = "open";
+     	menu = "open";
     } else {
       	$(this).parent().next(".mobile-nav").css("transform", "translate(0, -999%)");
       	menu = "close";
-    }
+	}
 });
 });
+
+
