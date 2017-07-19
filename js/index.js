f9de1e6cbe1b7e38cb4c00344b3cfdf1e1abcb83
izuzu3 <godgodgawd@gmail.com>
2017-07-19 10:06:54 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,12 +6,12 @@ $(".mobile-four .menu-toggle").hover(function() {
     if (menu === "close") {
       	$(this).parent().next(".mobile-nav").css("transform", "translate(0, 0)");
 		$(this).parent().next(".mobile-nav").css("transition-duration", "0.5s");
-		$(this).parent().next(".mobile-nav").css("transition-delay", "1s");
+		$(this).parent().next(".mobile-nav").css("transition-delay", "0.5s
 		menu = "open";
     } else {
       	$(this).parent().next(".mobile-nav").css("transform", "translate(0, -999%)");
-		$(this).parent().next(".mobile-nav").css("transition-duration", "1s");
-		$(this).parent().next(".mobile-nav").css("transition-delay", "1s");
+		$(this).parent().next(".mobile-nav").css("transition-duration", "0.5;
+		$(this).parent().next(".mobile-nav").css("transition-delay", "0.5;
 		menu = "close";
 	}
 });
